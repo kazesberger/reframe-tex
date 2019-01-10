b6726373790d635b5699e2d381b29fc669b7ba5a
kazesberger <klaus.azesberger@gmail.com>
2019-01-10 22:34:48 +0100

    Step1: add an input component

--- src/cljs/reframe_tex/views.cljs
+++ src/cljs/reframe_tex/views.cljs
@@ -3,9 +3,13 @@
    [re-frame.core :as re-frame]
    [reframe-tex.subs :as subs]))
 
+(defn demo-input []
+    [:input {:placeholder "your name goes here"
+             :type        "text"}])
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1 "Hello from " @name]]))
+     [:h1 "Hello from " @name]
+     [demo-input]]))
 
