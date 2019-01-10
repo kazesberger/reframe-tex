ed119d6b5217e8d71683e26d80af7ca0ecfded81
kazesberger <klaus.azesberger@gmail.com>
2019-01-10 22:44:14 +0100

    Step4: dispatch name-change event with changed input value

--- src/cljs/reframe_tex/views.cljs
+++ src/cljs/reframe_tex/views.cljs
@@ -1,13 +1,15 @@
 (ns reframe-tex.views
   (:require
    [re-frame.core :as re-frame]
-   [reframe-tex.subs :as subs]))
+   [reframe-tex.subs :as subs]
+   [reframe-tex.events :as events]))
 
 (defn demo-input []
   (let [name (re-frame/subscribe [::subs/name])]
     [:input {:placeholder "your name goes here"
              :type        "text"
-             :value       @name}]))
+             :value @name
+             :on-change   #(re-frame/dispatch [::events/name-change (-> % .-target .-value)])}]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
